c117ef3fdce3edf7a3baf7f7f63a347f4cf3edb7
machkouroke <machkour20.mo1@gmail.com>
2023-03-21 21:00:45 +0000

    Init commit

---
+++ src/main/java/com/lop/smartcitykhouribga/SmartCityKhouribgaApplication.java
@@ -0,0 +1,13 @@
+package com.lop.smartcitykhouribga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityKhouribgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCityKhouribgaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lop/smartcitykhouribga/SmartCityKhouribgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lop.smartcitykhouribga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityKhouribgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
