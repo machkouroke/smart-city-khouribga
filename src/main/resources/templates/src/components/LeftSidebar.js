b978fc958cb649572c13820628f9fb89c6853149
machkouroke <machkour20.mo1@gmail.com>
2023-03-29 23:40:52 +0000

    `feat` Add of navElement props in LeftSideBar

--- src/main/resources/templates/src/components/LeftSidebar.js
+++ src/main/resources/templates/src/components/LeftSidebar.js
@@ -1,7 +1,25 @@
 import '../style/scss/styles.scss';
+import {Menu} from "./Menu";
+
 const candy = "https://firebasestorage.googleapis.com/v0/b/cesam-website-374720.appspot.com/o/Pictures%2FUser%2Fcandyahogoudedji%40gmail.com.png?alt=media&token=6f132115-1634-4ec7-ab7d-91fbccbc1b2f";
 
-export function LeftSidebar() {
+export function LeftSidebar({activeNav}) {
+    const navElement = [
+        {
+            route: "/",
+            text: "Offres d'emploi",
+        },
+        {
+            route: "/edit_profile",
+            text: "Modifier mon profil",
+        },
+        {
+            route: "/applied_offer",
+            text: "Offres postulé",
+        }
+    ].map((element) => {
+        return Object.assign(element, {active: element.route === activeNav})
+    })
     return (
         <div className="col-lg-3 col-12">
             {/*Left Sidebar*/}
@@ -20,15 +38,11 @@ export function LeftSidebar() {
                     </div>
                 </div>
                 <div className="navigation">
-                    <button className="active"><i className="far fa-newspaper"></i><a className="text-dark"
-                                                                                      href="index.html">Offres
-                        d'emploi</a></button>
-                    <button><i className="far fa-user"></i><a className="text-dark" href="">Modifier mon
-                        profil</a></button>
+                    {
+                        navElement.map((element) =>
+                            <Menu route={element.route} active={element.active} text={element.text}/>)
+                    }
 
-                    <button><i className="far fa-bookmark"></i><a className="text-dark"
-                                                                  href="applied_offer.html">Offres
-                        postulé</a></button>
                 </div>
 
                 <div className="navbar-spacer"></div>
--- src/main/resources/templates/src/components/Main.js
+++ src/main/resources/templates/src/components/Main.js
@@ -3,19 +3,30 @@ import {LeftSidebar} from "./LeftSidebar";
 import {CreatePost} from "./CreatePost";
 import {UserPost} from "./UserPost";
 import {RightSideBar} from "./RightSideBar";
+import {NavBar} from "./NavBar";
+import React from "react";
 
-export function Main() {
+export function Main(props) {
+    console.log(props.data);
     return (
-        <div className="container">
-            <div className="row">
-                <LeftSidebar />
-                <div className="col-lg-6 col-12 timeline">
-                    <CreatePost />
-                    {/* Boucle sur les posts */}
-                    <UserPost />
+        <React.Fragment>
 
+            <NavBar/>
+
+            <div className="container">
+                <div className="row">
+                    <LeftSidebar activeNav={props.data === "all" ? "/" : "/applied_offer"
+                    }/>
+                    <div className="col-lg-6 col-12 timeline">
+                        <CreatePost/>
+                        {/* Boucle sur les posts */}
+                        <UserPost/>
+
+                    </div>
+                    <RightSideBar/>
                 </div>
-                <RightSideBar />
             </div>
-        </div>)
+        </React.Fragment>
+    )
 }
+
