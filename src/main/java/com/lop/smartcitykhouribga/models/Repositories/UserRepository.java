dbfe54599e1c015379fb252ebaa6017dda7ef4d6
machkouroke <machkour20.mo1@gmail.com>
2023-03-29 23:53:15 +0000

    `chore` Add of specific folder for repository

---
+++ src/main/java/com/lop/smartcitykhouribga/models/Repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.lop.smartcitykhouribga.models.Repositories;
+
+/* TODO: Ecrire le repository de la classe user les méthodes on vas en discuter fais moi signe au moment de les écrires */
+public class UserRepository {
+}
