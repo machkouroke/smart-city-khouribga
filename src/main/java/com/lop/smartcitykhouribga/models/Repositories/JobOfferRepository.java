39e408f13069f2e6ddf9ed8919c9b17ae10e1317
MorelK1 <kouhossounon.vianney@gmail.com>
2023-03-30 15:10:29 +0000

    `feat` adding extensions to repository interfaces

--- src/main/java/com/lop/smartcitykhouribga/models/Repositories/JobOfferRepository.java
+++ src/main/java/com/lop/smartcitykhouribga/models/Repositories/JobOfferRepository.java
@@ -1,7 +1,9 @@
 package com.lop.smartcitykhouribga.models.Repositories;
 
 import com.lop.smartcitykhouribga.models.Entities.JobOffer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface JobOfferRepository extends PagingAndSortingRepository<JobOffer, Long> {
+public interface JobOfferRepository extends PagingAndSortingRepository<JobOffer, Long>,
+        CrudRepository<JobOffer, Long> {
 }
