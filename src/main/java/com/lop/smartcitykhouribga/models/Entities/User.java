7049d61d3b577824213c6733b7bde5d3c0d7716e
MorelK1 <kouhossounon.vianney@gmail.com>
2023-03-30 10:11:43 +0000

    `feat` add user entity class

--- src/main/java/com/lop/smartcitykhouribga/models/Entities/User.java
+++ src/main/java/com/lop/smartcitykhouribga/models/Entities/User.java
@@ -1,5 +1,18 @@
 package com.lop.smartcitykhouribga.models.Entities;
 
+import lombok.*;
+
 /* TODO: Ecrire le code de la classe User */
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
+
+    private String id;
+    private String name;
+    private String surname;
+    private String mail;
+    private String cv;
+    private String role;
 }
