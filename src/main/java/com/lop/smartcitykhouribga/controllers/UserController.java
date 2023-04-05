29d6a564bed37af92a8f0272d76f98cef6848d8e
MorelK1 <kouhossounon.vianney@gmail.com>
2023-04-05 20:08:31 +0000

    `feat` add get user function

--- src/main/java/com/lop/smartcitykhouribga/controllers/UserController.java
+++ src/main/java/com/lop/smartcitykhouribga/controllers/UserController.java
@@ -1,4 +1,27 @@
 package com.lop.smartcitykhouribga.controllers;
 
+import com.lop.smartcitykhouribga.models.Entities.User;
+import com.lop.smartcitykhouribga.models.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable long id){
+        return userService.findById(id);
+    }
+
+
+
 }
