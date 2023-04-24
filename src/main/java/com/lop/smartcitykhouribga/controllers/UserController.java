07230fdeccb3f5ca71067d52d65e5398e7b4f6e4
MorelK1 <kouhossounon.vianney@gmail.com>
2023-04-24 18:22:09 +0100

    `feat` add user registration function

--- src/main/java/com/lop/smartcitykhouribga/controllers/UserController.java
+++ src/main/java/com/lop/smartcitykhouribga/controllers/UserController.java
@@ -4,8 +4,11 @@ import com.lop.smartcitykhouribga.models.Entities.JobOffer;
 import com.lop.smartcitykhouribga.models.Entities.User;
 import com.lop.smartcitykhouribga.models.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Set;
 
 
@@ -15,6 +18,18 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> saveUser(@ModelAttribute User user,
+                                           @RequestParam("cv")MultipartFile cv,
+                                           @RequestParam("photo") MultipartFile photo) throws IOException {
+        userService.uploadUserCV(user, cv);
+        userService.uploadUserPhoto(user, photo);
+        user.setRole("recruiter");
+
+        userService.save(user);
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable Long id){
