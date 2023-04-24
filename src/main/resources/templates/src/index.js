43440bd455f2715031d173349185d37de670ca57
machkouroke <machkour20.mo1@gmail.com>
2023-04-24 12:27:53 +0100

    `feat` Ajout du logging Logrocket

--- src/main/resources/templates/src/index.js
+++ src/main/resources/templates/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import {configureStore} from '@reduxjs/toolkit'
 import {Provider} from 'react-redux';
 import reducers from './slices/index';
-
+import LogRocket from 'logrocket';
+LogRocket.init('iectio/smart-city');
 const store = configureStore({
     reducer: reducers
 })
@@ -17,7 +18,5 @@ root.render(
     </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
