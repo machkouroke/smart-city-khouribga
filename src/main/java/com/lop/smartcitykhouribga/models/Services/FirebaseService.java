df9892620bf97b89e9a89f5d113d0d5df28a3b4c
machkouroke <machkour20.mo1@gmail.com>
2023-04-24 16:07:38 +0100

    `feat` Ajout de classe FirebaseService

---
+++ src/main/java/com/lop/smartcitykhouribga/models/Services/FirebaseService.java
@@ -0,0 +1,40 @@
+package com.lop.smartcitykhouribga.models.Services;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FirebaseService {
+    private static final String CREDENTIALS_PATH = "credentials.json";
+    private static final String GCLOUD_BUCKET = "smart-city-915d9.appspot.com";
+
+    public FirebaseService() {
+        try {
+            /* print current path */
+            System.out.println("Current Path: " + System.getProperty("user.dir"));
+
+
+            FileInputStream serviceAccount = new FileInputStream(CREDENTIALS_PATH);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(credentials)
+                    .setStorageBucket(GCLOUD_BUCKET)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bucket getBucket() {
+        return StorageClient.getInstance().bucket();
+    }
+}
