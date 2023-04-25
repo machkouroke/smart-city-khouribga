4dcc27336f0e6fc0a948d34214bc5c01c0fe5418
machkouroke <machkour20.mo1@gmail.com>
2023-04-25 19:29:24 +0100

    `feat` Minor

--- src/main/java/com/lop/smartcitykhouribga/controllers/JobOfferController.java
+++ src/main/java/com/lop/smartcitykhouribga/controllers/JobOfferController.java
@@ -29,38 +29,38 @@ public class JobOfferController {
 
 
     @PostMapping("/add")
-    public void addOffer(@AuthenticationPrincipal User details,
-                         @ModelAttribute OfferDTO dto){
+    public void addOffer(User details,
+                         @ModelAttribute OfferDTO dto) {
 
         User user = details;
         dto.setPostedAt(new Date());
 
-        JobOffer offer= offerService.save(offerService.convertToEntity(dto));
+        JobOffer offer = offerService.save(offerService.convertToEntity(dto));
 
 
-        UserOfferRelation uor= new UserOfferRelation(user, offer);
-        uor.setId(new UserOfferRelationKeys(user.getId(),offer.getId(),"posted"));
+        UserOfferRelation uor = new UserOfferRelation(user, offer);
+        uor.setId(new UserOfferRelationKeys(user.getId(), offer.getId(), "posted"));
         userService.saveRelation(uor);
     }
 
 
     @PostMapping("/reaction")
-    public void toggleReaction(@AuthenticationPrincipal UserDetailsImpl details,
-                          @RequestParam("id") Long id, @RequestParam("type") String type){
-        User user = details.getUser(userRepository);
-        JobOffer offer= offerService.findById(id);
+    public void toggleReaction(User details,
+                               @RequestParam("id") Long id, @RequestParam("type") String type) {
+        User user = details;
+        JobOffer offer = offerService.findById(id);
 
-        UserOfferRelation uor= new UserOfferRelation(user, offer);
-        uor.setId(new UserOfferRelationKeys(user.getId(),offer.getId(),type));
+        UserOfferRelation uor = new UserOfferRelation(user, offer);
+        uor.setId(new UserOfferRelationKeys(user.getId(), offer.getId(), type));
 
-        UserOfferRelation uor1= new UserOfferRelation(user, offer);
-        uor1.setId(new UserOfferRelationKeys(user.getId(),offer.getId(),type));
+        UserOfferRelation uor1 = new UserOfferRelation(user, offer);
+        uor1.setId(new UserOfferRelationKeys(user.getId(), offer.getId(), type));
 
 
-        if(user.getRelatedOffers().stream().anyMatch(r->r.equals(uor))){
+        if (user.getRelatedOffers().stream().anyMatch(r -> r.equals(uor))) {
             userService.deleteRelation(uor);
             System.out.println("It contains");
-        }else {
+        } else {
             userService.saveRelation(uor);
             System.out.println("No");
         }
@@ -68,27 +68,27 @@ public class JobOfferController {
     }
 
     @GetMapping
-    public List<JobOffer> getAllOffers(){
+    public List<JobOffer> getAllOffers() {
         return offerService.findAll();
     }
 
     @GetMapping("/{id}")
-    public JobOffer getOffer(@PathVariable Long id){
+    public JobOffer getOffer(@PathVariable Long id) {
         return offerService.findById(id);
     }
 
     @GetMapping("/{id}/users/{type}")
-    public Set<User> getRelatedOffers(@PathVariable Long id, @PathVariable String type){
+    public Set<User> getRelatedOffers(@PathVariable Long id, @PathVariable String type) {
         return offerService.findUsersRelatedToOffer(id, type);
     }
 
     @GetMapping("/search/{word}")
-    public List<JobOffer> search(@PathVariable String word){
+    public List<JobOffer> search(@PathVariable String word) {
         return offerService.searchOffers(word);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteOffers(@PathVariable Long id){
+    public void deleteOffers(@PathVariable Long id) {
         offerService.deletebyId(id);
     }
 }
