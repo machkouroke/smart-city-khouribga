d1f378ecb4e41d867d35c7e942ad8bea02d1e136
MorelK1 <kouhossounon.vianney@gmail.com>
2023-04-25 21:45:46 +0100

    `feat` deleting files from temp directory

--- src/main/java/com/lop/smartcitykhouribga/utilities/FileUtility.java
+++ src/main/java/com/lop/smartcitykhouribga/utilities/FileUtility.java
@@ -38,10 +38,11 @@ public class FileUtility {
         if (validateFiles(cv)) {
 
             String extension = FilenameUtils.getExtension(cv.getOriginalFilename());
-            String cvPathname = System.getProperty("user.dir")+ File.separator+ path + File.separator + user.getName() + "Cv." + extension;
+            String cvPathname = System.getProperty("user.dir")+ File.separator+ path + File.separator + user.getMail() + "Cv." + extension;
             System.out.println("CVPathname: " + cvPathname );
             cv.transferTo(new File(cvPathname));
             firebaseService.uploadFile(Path.of(cvPathname), "User/Cv");
+            Files.delete(Path.of(cvPathname));
         }
 
     }
@@ -55,11 +56,12 @@ public class FileUtility {
         }
         if (validateFiles(photo)) {
             String extension = FilenameUtils.getExtension(photo.getOriginalFilename());
-            String photoPathname = System.getProperty("user.dir")+ File.separator+ path + File.separator + user.getName() + "Picture." + extension;
+            String photoPathname = System.getProperty("user.dir")+ File.separator+ path + File.separator + user.getMail() + "Picture." + extension;
             System.out.println(photoPathname);
             photo.transferTo(new File(photoPathname));
 
             firebaseService.uploadFile(Path.of(photoPathname), "User/Pictures");
+            Files.delete(Path.of(photoPathname));
         }
     }
 
