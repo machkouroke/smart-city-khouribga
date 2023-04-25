85705d2f1ebb9bbb6ff45878dbbe6d62750c7444
MorelK1 <kouhossounon.vianney@gmail.com>
2023-04-25 19:30:41 +0100

    `feat` deleterelation by id

--- src/main/java/com/lop/smartcitykhouribga/controllers/UserController.java
+++ src/main/java/com/lop/smartcitykhouribga/controllers/UserController.java
@@ -2,9 +2,12 @@ package com.lop.smartcitykhouribga.controllers;
 
 import com.lop.smartcitykhouribga.models.Entities.JobOffer;
 import com.lop.smartcitykhouribga.models.Entities.User;
+import com.lop.smartcitykhouribga.models.Entities.UserDetailsImpl;
+import com.lop.smartcitykhouribga.models.Repositories.UserRepository;
 import com.lop.smartcitykhouribga.models.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +22,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping("/register")
     public ResponseEntity<Object> saveUser(@ModelAttribute User user,
                                            @RequestParam("cv")MultipartFile cv,
@@ -32,9 +38,11 @@ public class UserController {
         return userService.findById(id);
     }
 
-    @GetMapping("/{id}/offers/{type}")
-    public Set<JobOffer> getRelatedOffers(@PathVariable Long id, @PathVariable String type){
-        return userService.findOffersRelatedToUser(id, type);
+    @GetMapping("/offers/{type}")
+    public ResponseEntity<Set<JobOffer>> getRelatedOffers(@AuthenticationPrincipal UserDetailsImpl details,
+                                          @PathVariable String type){
+        User user= details.getUser(userRepository);
+        return ResponseEntity.ok().body(userService.findOffersRelatedToUser(user.getId(), type));
     }
 
 
--- src/main/java/com/lop/smartcitykhouribga/models/Repositories/RelationRepository.java
+++ src/main/java/com/lop/smartcitykhouribga/models/Repositories/RelationRepository.java
@@ -1,9 +1,12 @@
 package com.lop.smartcitykhouribga.models.Repositories;
 
 import com.lop.smartcitykhouribga.models.Entities.UserOfferRelation;
+import com.lop.smartcitykhouribga.models.Keys.UserOfferRelationKeys;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface RelationRepository extends PagingAndSortingRepository<UserOfferRelation, Long>,
-        CrudRepository<UserOfferRelation, Long> {
+import java.util.Optional;
+
+public interface RelationRepository extends PagingAndSortingRepository<UserOfferRelation, UserOfferRelationKeys>,
+        CrudRepository<UserOfferRelation, UserOfferRelationKeys> {
 }
